bcf65442acc6212c536cf0b9dc8b25b0b171dec1
Fco <fcodelgado@Fcos-MacBook-Pro.local>
2024-02-09 11:47:04 -0600

    First draft for docker-compose idpartner-hosted trust-platform

---
+++ idpartner-hosted/scripts/setup_local.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Load ENV vars
+source .env
+
+ROOT_DIR=`dirname $(pwd)`
+MIGRATIONS_IMAGE=mock-bank-migrations
+
+############################################################################
+#
+#           MAIN
+#
+############################################################################
+
+echo "Setting up Trust Platform locally..."
+
+# Start services
+docker compose up --detach --remove-orphans --wait
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+# Give the services a few seconds to come up
+echo "Preparing to run migrations and seeds please wait..."
+sleep 30
+
+# Run migrations
+echo "docker run --rm -e SQL_USER=${SQL_USER} -e SQL_PASSWORD=${SQL_PASSWORD} -e SQL_DATABASE=${SQL_DATABASE} -e SQL_HOST=${SQL_HOST} ${MIGRATIONS_IMAGE}"
+docker run --rm -e SQL_USER=${SQL_USER} -e SQL_PASSWORD=${SQL_PASSWORD} -e SQL_DATABASE=${SQL_DATABASE} -e SQL_HOST=${SQL_HOST} ${MIGRATIONS_IMAGE}
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+# Run seeds
+docker run --rm -e SQL_USER=${SQL_USER} -e SQL_PASSWORD=${SQL_PASSWORD} -e SQL_DATABASE=${SQL_DATABASE} -e SQL_HOST=${SQL_HOST} ${MIGRATIONS_IMAGE} yarn seed
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+# Stop services
+docker compose down
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+echo "Setup FINISHED."
+echo ""
+echo "From this point onward you should run 'docker compose up' to start the Trust Platform"
+echo ""
+echo ""
